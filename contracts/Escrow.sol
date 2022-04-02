0b41aa545d0eb232b723bc74eaff924701b06c0f
Evan W <ewitulsk@calpoly.edu>
2022-04-02 22:54:32 +0200

    quality of life

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -14,24 +14,46 @@ contract Escrow{
 
     uint256 buyerDep = 0;
     uint256 sellerDep = 0;
+    address buyer = address(0); //buyer address 
+    address seller = address(0); // seller address 
+
 
     function buyerDeposit(uint256 amount) public {
         require(amount == itemValue*2);
         cUSD.transferFrom(msg.sender, address(this), amount);
         buyerDep+=amount;
+        buyer = msg.sender; 
     }
 
     function sellerDeposit(uint256 amount) public {
         require(amount == itemValue*2);
-        cUSD.transferFrom(msg.sender, address(this), amount);
+        cUSD.transferFrom(msg.sender, address(this), amount);   //Note: need to approve
         sellerDep+=amount;
+        seller = msg.sender; 
     }
 
     function releaseEscrow() public {
+        require(seller != address(0)); //buyer address 
+        require(buyer != address(0)); // seller address 
+
         //require both buyer and seller deposited 2x inital item value
+        require(buyerDep == itemValue*2);
+        require(sellerDep == itemValue*2);
+
         //move .5 the buyers deposit to the seller
+        cUSD.transferFrom(buyer, seller, buyerDep / 2);
+        sellerDep -= buyerDep / 2;
+
         //release both buyer and seller deposits
+        cUSD.transferFrom(address(this), buyer, buyerDep);
+        cUSD.transferFrom(address(this), seller, sellerDep);
+    }
+
+    function getSellerDeposit() public view returns (uint256) {
+        return sellerDep;
+    }
+
+    function getBuyerDeposit() public view returns (uint256) {
+        return buyerDep;
     }
 }
-
-
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,25 +1,42 @@
 pragma solidity ^0.8.0;
 
+import './Escrow.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
 contract Marketplace{
+    enum ListingStatus{ AVAILABLE, SOLD, RECIEVED, INACTIVE }
     struct Listing{
         string name;
         string description;
         uint256 price;
         string ipfsHash;
-        bool sold;
+        ListingStatus status;
         uint256 itemId;
         address escrowAddr;
     }
+
+    constructor(address _cUSDAddr){
+        cUSDAddr = _cUSDAddr;
+    }
+
+    address cUSDAddr;
 
     //userWalletAddr => userListings
     mapping(address => Listing[]) userListings;
 
     //listingId => Listing
-    mapping(uint256 => Listing) listings;
+    mapping(uint256 => Listing) listingsMap;
+
+    mapping(address => Listing[]) userPurchases;
+
+    Listing[] listingsArr;
 
     uint256 idCounter = 0;
 
     event newListing(string name, string description, uint256 price, string ipfsHash, bool sold, uint256 itemId);
+
+    event newEscrow(address contractAddr, uint256 itemId);
 
     function newItem(string memory name, string memory description, uint256 price, string memory ipfsHash) public {
         Listing memory listing;
@@ -27,15 +44,30 @@ contract Marketplace{
         listing.description = description;
         listing.price = price;
         listing.ipfsHash = ipfsHash;
-        listing.sold = false;
+        listing.status = ListingStatus.AVAILABLE;
         listing.itemId = idCounter;
 
-        //Deploy new Escrow contract.
-        //listing.escrowAddr = 
-        //call sellerDeposit()
+        // Deploy new escrow instance
+        // Set escrow contract address
+        Escrow escrowContract = new Escrow(listing.price, cUSDAddr);    
+        listing.escrowAddr = address(escrowContract);   
+        emit newEscrow(listing.escrowAddr, listing.itemId);
 
-        listings[idCounter] = listing;
+
+        // Connect to cUSD and transfer funds to marketplace contract
+        ERC20 cUSD = ERC20(cUSDAddr);
+        require(cUSD.balanceOf(msg.sender) >= listing.price * 2);
+        cUSD.transferFrom(msg.sender, address(this), listing.price * 2);
+
+        // Call sellerDeposit function
+        // Have to approve transfer first
+        cUSD.approve(listing.escrowAddr, listing.price * 2);    
+        escrowContract.sellerDeposit(listing.price * 2);
+        require(escrowContract.getSellerDeposit() == listing.price * 2, "Seller deposit failed");
+
+        listingsMap[idCounter] = listing;
         userListings[msg.sender].push(listing);
+        listingsArr.push(listing);
 
         idCounter++;
         
@@ -46,14 +78,44 @@ contract Marketplace{
         return userListings[msg.sender];
     }
 
-    function purchase(uint256 tokenId) public{
-        //call buyerDeposit from Escrow
+    function purchase(uint256 listingId) public{
+        Escrow escrowContract = Escrow(listingsMap[listingId].escrowAddr);
+        ERC20 cUSD = ERC20(cUSDAddr);
+
+        // Get cUSD from buyer
+        require(cUSD.balanceOf(msg.sender) >= listingsMap[listingId].price * 2);
+        cUSD.transferFrom(msg.sender, address(this), listingsMap[listingId].price * 2);
+
+        // Call buyerDeposit
+        cUSD.approve(listingsMap[listingId].escrowAddr, listingsMap[listingId].price * 2);    
+        escrowContract.buyerDeposit(listingsMap[listingId].price * 2);
+        userPurchases[msg.sender].push(listingsMap[listingId]);
+        require(escrowContract.getBuyerDeposit() == listingsMap[listingId].price * 2, "Buyer deposit failed");
     }
 
-    function releaseEscrow() public {
+    // listing id as a parameter 
+    // access it via listing id 
+    function releaseEscrow( uint256 listingId) public {
         //call releaseEscrow from Escrow
+        address escrow_add = listingsMap[listingId].escrowAddr;
+        Escrow escrowContract = Escrow(escrow_add);  
+        escrowContract.releaseEscrow();
+    }
+
+    function getListingInfo(uint256 listingId) public view returns (Listing memory) {
+        return listingsMap[listingId];
+    }
+
+    function getListings() public view returns(Listing[] memory){
+        return listingsArr;
+    }
+
+    function deactivateListing(uint256 itemId) public {
+        listingsMap[itemId].status = ListingStatus.INACTIVE;
+    }
+
+    function getUserListings(address wallet) public view returns(Listing[] memory){
+        return userPurchases[wallet];
     }
 
 }
-
-
