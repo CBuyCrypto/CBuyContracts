8bacce0ead6d1fcab4cec8d0672511dd06d52fd1
Daniel Leavitt <daniel.7039@hotmail.com>
2022-04-02 15:28:13 -0700

    Authorized users and all tested on remix

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -7,29 +7,35 @@ contract Escrow{
     uint256 itemValue;
     IERC20 cUSD;
 
-    constructor(uint256 _itemValue, address _cUSDAddr){
+    constructor(uint256 _itemValue, address _cUSDAddr, address marketPlaceAddr){
         itemValue = _itemValue;
         cUSD = IERC20(_cUSDAddr);
+        cBuy = marketPlaceAddr;
     }
 
     uint256 buyerDep = 0;
     uint256 sellerDep = 0;
     address buyer = address(0); //buyer address 
     address seller = address(0); // seller address 
+    // uint256 escrowAgentPercentage = 50000000000000000; // 5%
+    uint256 cBuyFraction = 40; // 5%
+    uint256 feeFraction = (cBuyFraction) * 2;
+    address cBuy;
 
 
-    function buyerDeposit(uint256 amount) public {
+
+    function buyerDeposit(uint256 amount, address _buyer) public {
         require(amount == itemValue*2);
         cUSD.transferFrom(msg.sender, address(this), amount);
         buyerDep+=amount;
-        buyer = msg.sender; 
+        buyer = _buyer; 
     }
 
-    function sellerDeposit(uint256 amount) public {
+    function sellerDeposit(uint256 amount, address _seller) public {
         require(amount == itemValue*2);
         cUSD.transferFrom(msg.sender, address(this), amount);   //Note: need to approve
         sellerDep+=amount;
-        seller = msg.sender; 
+        seller = _seller; 
     }
 
     function releaseEscrow() public {
@@ -40,13 +46,20 @@ contract Escrow{
         require(buyerDep == itemValue*2);
         require(sellerDep == itemValue*2);
 
-        //move .5 the buyers deposit to the seller
-        cUSD.transferFrom(buyer, seller, buyerDep / 2);
-        sellerDep -= buyerDep / 2;
+        // //move .5 the buyers deposit to the seller
+        // cUSD.transferFrom(buyer, seller, buyerDep / 2); //Note: make sure to approv this
+        // sellerDep -= buyerDep / 2;
 
-        //release both buyer and seller deposits
-        cUSD.transferFrom(address(this), buyer, buyerDep);
-        cUSD.transferFrom(address(this), seller, sellerDep);
+        // //release both buyer and seller deposits
+        // cUSD.transferFrom(address(this), buyer, buyerDep);
+        // cUSD.transferFrom(address(this), seller, sellerDep);
+
+        cUSD.transfer(seller, itemValue + itemValue + itemValue - itemValue / feeFraction);   // sale + deposit - 2.5%
+        cUSD.transfer(buyer, itemValue - itemValue / feeFraction);                // deposit - 2.5%
+        cUSD.transfer(cBuy, itemValue / cBuyFraction);                            // 5%
+
+        sellerDep = 0;
+        buyerDep = 0;
     }
 
     function getSellerDeposit() public view returns (uint256) {
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -13,20 +13,27 @@ contract Marketplace{
         bool sold;
         uint256 itemId;
         address escrowAddr;
+        uint256 createdOn;
     }
 
     address cUSDAddr;
-
     //userWalletAddr => userListings
     mapping(address => Listing[]) userListings;
 
     //listingId => Listing
     mapping(uint256 => Listing) listings;
 
+    address[] private authorizedUsers;
+    mapping(address => uint256) private authorizedUserBalance;
+
     uint256 idCounter = 0;
+
+    uint256 cBuyPercentage = 50000000000000000; // 5%
+
 
     constructor(address _cUSDAddr){
         cUSDAddr = _cUSDAddr;
+        authorizedUsers.push(msg.sender);
     }
 
     event newListing(string name, string description, uint256 price, string ipfsHash, bool sold, uint256 itemId);
@@ -41,10 +48,11 @@ contract Marketplace{
         listing.ipfsHash = ipfsHash;
         listing.sold = false;
         listing.itemId = idCounter;
+        listing.createdOn = block.number;
 
         // Deploy new escrow instance
         // Set escrow contract address
-        Escrow escrowContract = new Escrow(listing.price, cUSDAddr);    
+        Escrow escrowContract = new Escrow(listing.price, cUSDAddr, address(this));    
         listing.escrowAddr = address(escrowContract);   
         emit newEscrow(listing.escrowAddr, listing.itemId);
 
@@ -57,7 +65,7 @@ contract Marketplace{
         // Call sellerDeposit function
         // Have to approve transfer first
         cUSD.approve(listing.escrowAddr, listing.price * 2);    
-        escrowContract.sellerDeposit(listing.price * 2);
+        escrowContract.sellerDeposit(listing.price * 2, msg.sender);
         require(escrowContract.getSellerDeposit() == listing.price * 2, "Seller deposit failed");
 
         listings[idCounter] = listing;
@@ -74,29 +82,64 @@ contract Marketplace{
 
     function purchase(uint256 listingId) public{
         Escrow escrowContract = Escrow(listings[listingId].escrowAddr);
-        ERC20 cUSD = ERC20(cUSDAddr);
 
         // Get cUSD from buyer
+        ERC20 cUSD = ERC20(cUSDAddr);
         require(cUSD.balanceOf(msg.sender) >= listings[listingId].price * 2);
         cUSD.transferFrom(msg.sender, address(this), listings[listingId].price * 2);
 
         // Call buyerDeposit
         cUSD.approve(listings[listingId].escrowAddr, listings[listingId].price * 2);    
-        escrowContract.buyerDeposit(listings[listingId].price * 2);
+        escrowContract.buyerDeposit(listings[listingId].price * 2, msg.sender);
         require(escrowContract.getBuyerDeposit() == listings[listingId].price * 2, "Buyer deposit failed");
+
+        listings[listingId].sold = true;
     }
 
     // listing id as a parameter 
     // access it via listing id 
-    function releaseEscrow( uint256 listingid) public {
+    function releaseEscrow( uint256 listingId) public {
         //call releaseEscrow from Escrow
-        address escrow_add = listings[listingid].escrowAddr;
+        address escrow_add = listings[listingId].escrowAddr;
         Escrow escrowContract = Escrow(escrow_add);  
         escrowContract.releaseEscrow();
+        _receiveFee(listings[listingId].price / cBuyPercentage);
     }
 
-    function getListingInfo(uint256 listingId) public returns (listing) {
+    function getListingInfo(uint256 listingId) public view returns (Listing memory) {
         return listings[listingId];
+    }
+
+    function _receiveFee(uint256 totalFee) internal {
+        uint256 feePerUser = totalFee / authorizedUsers.length;
+        for(uint256 i = 0; i < authorizedUsers.length; i++){
+            authorizedUserBalance[authorizedUsers[i]] += feePerUser;
+        }
+    }
+
+    function withdraw() public {
+        require(_isAuthorized(msg.sender), "You are not an authorized user");
+        ERC20 cUSD = ERC20(cUSDAddr);
+        cUSD.transfer(msg.sender, authorizedUserBalance[msg.sender]);
+        authorizedUserBalance[msg.sender] = 0;
+    }
+
+    function authorizeUser(address newUser) public {
+        require(_isAuthorized(msg.sender), "You are not an authorized user");
+        authorizedUsers.push(newUser);
+    }
+
+    function _isAuthorized(address user) internal view returns (bool){
+        for(uint256 i = 0; i < authorizedUsers.length; i++){
+            if(authorizedUsers[i] == user){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkBalance() public returns (uint256) {
+        return authorizedUserBalance[msg.sender];
     }
 
 }
